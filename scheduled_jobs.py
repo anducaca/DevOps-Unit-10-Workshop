19ec28010e385e64aefc7ee7248acf827416dda8
anducaca <95718224+anducaca@users.noreply.github.com>
2026-04-07 15:45:04 +0100

    Implement error handling in process_orders function
    Added error handling for order processing and logging.

--- scheduled_jobs.py
+++ scheduled_jobs.py
@@ -20,7 +20,7 @@ def process_orders(app):
         orders = get_queue_of_orders_to_process()
         if len(orders) == 0:
             return
-
+try:
         order = orders[0]
 
         payload = {
@@ -39,9 +39,67 @@ def process_orders(app):
 
         order.set_as_processed()
         save_order(order)
+except:
+    from data.database import save_order, get_all_orders
+from products import create_product_download
+from apscheduler.schedulers.background import BackgroundScheduler
+import requests
+
+
+def initialise_scheduled_jobs(app):
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(
+        func=process_orders,
+        args=[app],
+        trigger="interval",
+        seconds=app.config["SCHEDULED_JOB_INTERVAL_SECONDS"],
+    )
+    scheduler.start()
+
+
+def process_orders(app):
+    with app.app_context():
+        orders = get_queue_of_orders_to_process()
+        if len(orders) == 0:
+            return
+try:
+        order = orders[0]
+
+        payload = {
+            "product": order.product,
+            "customer": order.customer,
+            "date": order.date_placed.isoformat(),
+        }
+
+        response = requests.post(
+            app.config["FINANCE_PACKAGE_URL"] + "/ProcessPayment",
+            json=payload
+        )
+        app.logger.info("Response from endpoint: " + response.text)
+        
+        response.raise_for_status()
+
+        order.set_as_processed()
+        save_order(order)
+except:
+    app.logger.exception("Error processing order {id}".format(id = order.id))
+    
+def get_queue_of_orders_to_process():
+    allOrders = get_all_orders()
+    queuedOrders = filter(lambda order: order.date_processed == None, allOrders)
+    sortedQueue = sorted(queuedOrders, key= lambda order: order.date_placed)
+    return list(sortedQueue)
+
+
+
+
+
 
 def get_queue_of_orders_to_process():
     allOrders = get_all_orders()
     queuedOrders = filter(lambda order: order.date_processed == None, allOrders)
     sortedQueue = sorted(queuedOrders, key= lambda order: order.date_placed)
     return list(sortedQueue)
+
+
+
